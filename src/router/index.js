17eaba54b7da97f5a022cb1e0e7c7ab879dbf01b
Paul <frankypie202@gmail.com>
2021-11-03 16:01:24 +0100

    impressum seite

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "home" */ '../views/Home.vue')
+  },
+  {
+    path: '/impressum',
+    name: 'Impressum',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "home" */ '../views/Impressum.vue')
   }
 ]
 
---
+++ src/views/Impressum.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container class="impressum__container">
+    <h1 class="mt-5">Impressum</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Impressum',
+  components: {
+  },
+  metaInfo () {
+    return {
+    }
+  },
+  computed: {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.impressum__container {
+  position: relative;
+  text-align: center;
+}
+</style>
