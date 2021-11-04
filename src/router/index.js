32fc1fb9a08dd26e5d6e4bc1c15b49ada40b1572
Paul <frankypie202@gmail.com>
2021-11-04 10:48:46 +0100

    imprint done with Text

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "home" */ '../views/Impressum.vue')
+      import(/* webpackChunkName: "impressum" */ '../views/Impressum.vue')
   }
 ]
 
