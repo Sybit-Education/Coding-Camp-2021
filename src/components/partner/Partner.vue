5b96c0e34e57b6cde3a25677c1475e2f01d847b5
Viktor-creative <93599776+Viktor-creative@users.noreply.github.com>
2021-11-04 11:33:35 +0100

    Partner slides

--- src/components/partner/Partner.vue
+++ src/components/partner/Partner.vue
@@ -1,49 +1,39 @@
 <template>
 <div>
-  <h2>Partner</h2>
+  <h2>Partner:</h2>
     <v-carousel
     cycle
-    height="100"
+    height="200"
+    width="200"
     hide-delimiter-background
     hide-delimiters
     show-arrows-on-hover
   >
     <v-carousel-item
-      v-for="(slide, i) in slides"
-      :key="i"
+      v-for="partner in partnerList"
+      :key="partner.id"
     >
-        <v-row
-          class="fill-height"
-          align="center"
-          justify="center"
-        >
-          <div class="text-h2">
-            {{ slide }} Slide
-          </div>
-        </v-row>
+    <div class="d-flex justify-center">
+      <v-img :src="partner.logo[0].url" contain width="200" height="200" />
+    </div>
     </v-carousel-item>
   </v-carousel>
 </div>
 </template>
 <script>
+import partnerService from '@/services/partner.service'
 export default {
   data () {
     return {
-      colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4'
-      ],
-      slides: [
-        'First',
-        'Second',
-        'Third',
-        'Fourth',
-        'Fifth'
-      ]
+      partnerList:[]
+      
+      
     }
+  },
+  created() {
+    partnerService.getPartnerRecords().then(list => {
+      this.partnerList = list
+    })
   }
 }
 </script>
