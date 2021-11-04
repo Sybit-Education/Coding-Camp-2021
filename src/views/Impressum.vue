2d50ff71c5470df3c2156ac634471680f34ac66f
Paul <frankypie202@gmail.com>
2021-11-04 09:21:22 +0100

    Impressum done with text

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -1,25 +1,27 @@
 <template>
   <v-container class="impressum__container">
     <h1 class="mt-5">Impressum</h1>
+    <div>
+      <h3>Sybit GmbH</h3>
+        <h3>Sankt-Johannis-Str. 1 - 5</h3>
+        <h3>78315 Radolfzell</h3>
+        <h3>Tel. +49 7732 9508-0</h3>
+        <h3>E-Mail: karriere@sybit.de</h3>
+    </div>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'Impressum',
-  components: {
+  name: "Impressum",
+  components: {},
+  metaInfo() {
+    return {};
   },
-  metaInfo () {
-    return {
-    }
-  },
-  computed: {
-  },
-  mounted () {
-  },
-  methods: {
-  }
-}
+  computed: {},
+  mounted() {},
+  methods: {},
+};
 </script>
 
 <style scoped>
