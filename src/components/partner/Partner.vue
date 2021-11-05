6f0462f8f56673cb296e4db33e78112db7b494cd
MorrisBieg <93599697+MorrisBieg@users.noreply.github.com>
2021-11-05 09:46:51 +0100

    style

--- src/components/partner/Partner.vue
+++ src/components/partner/Partner.vue
@@ -1,49 +1,46 @@
 <template>
-<div>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
+  <div class="partner-space">
+    <p>Unsere Partner:</p>
     <v-carousel
-    cycle
-    height="140"
-    width="140"
-    hide-delimiter-background
-    hide-delimiters
-    show-arrows-on-hover
-  >
-    <v-carousel-item
-      v-for="partner in partnerList"
-      :key="partner.id"
+      cycle
+      height="140"
+      width="140"
+      hide-delimiter-background
+      hide-delimiters
+      show-arrows-on-hover
     >
-    <div class="d-flex justify-center">
-      <a :href="partner.website">
-      <v-img :src="partner.logo[0].url" contain width="140" height="140" />
-      </a>
-    </div>
-    </v-carousel-item>
-  </v-carousel>
-  <p>
-    Partner:
-  </p>
-</div>
+      <v-carousel-item v-for="partner in partnerList" :key="partner.id">
+        <div class="d-flex justify-center">
+          <a :href="partner.website">
+            <v-img
+              :src="partner.logo[0].url"
+              contain
+              width="140"
+              height="140"
+            />
+          </a>
+        </div>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 <script>
-import partnerService from '@/services/partner.service'
+import partnerService from "@/services/partner.service";
 export default {
-  data () {
+  data() {
     return {
-      partnerList: []
-
-    }
+      partnerList: [],
+    };
   },
-  created () {
-    partnerService.getPartnerRecords().then(list => {
-      this.partnerList = list
-    })
-  }
-}
+  created() {
+    partnerService.getPartnerRecords().then((list) => {
+      this.partnerList = list;
+    });
+  },
+};
 </script>
+<style scoped>
+.partner-space{
+  margin-top: 120px !important;
+}
+</style>
