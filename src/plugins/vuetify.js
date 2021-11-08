ac1edef2018807914594ae066e68601f28370437
Stephan Strittmatter <stephan@st-strittmatter.name>
2021-11-08 10:46:50 +0100

    add col in hex

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -9,7 +9,7 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: colors.amber.darken4,
+        primary: colors.amber.darken4, // #FF6F00
         onprimary: colors.grey.darken4,
         secondary: colors.lightGreen.darken3,
         onsecondary: colors.grey.darken4,
