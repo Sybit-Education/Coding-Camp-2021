ea99867035cff731e77bd282ae4c750138c215a7
Stephan Strittmatter <stephan@st-strittmatter.name>
2021-11-10 17:14:04 +0100

    Revert "corrected and unified manifest generation"
    This reverts commit b0e7754b37e6019b1de96eaa91ec30e7c467608c.

--- vue.config.js
+++ vue.config.js
@@ -5,90 +5,7 @@ module.exports = {
     themeColor: '#FF6F00',
     msTileColor: '#FF6F00',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black-translucent',
-    manifestOptions: {
-      background_color: '#FF6F00',
-      display: 'standalone',
-      orientation: 'portrait',
-      description: 'Finde heraus, was in welchen Müll gehört.',
-      permissions: [
-        'geolocation'
-      ],
-      icons: [
-        {
-          src: './img/icons/android-chrome-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/android-chrome-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/android-chrome-maskable-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-          purpose: 'maskable'
-        },
-        {
-          src: './img/icons/android-chrome-maskable-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable'
-        },
-        {
-          src: './img/icons/apple-touch-icon-60x60.png',
-          sizes: '60x60',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/apple-touch-icon-76x76.png',
-          sizes: '76x76',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/apple-touch-icon-120x120.png',
-          sizes: '120x120',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/apple-touch-icon-152x152.png',
-          sizes: '152x152',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/apple-touch-icon-180x180.png',
-          sizes: '180x180',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/apple-touch-icon.png',
-          sizes: '180x180',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/favicon-16x16.png',
-          sizes: '16x16',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/favicon-32x32.png',
-          sizes: '32x32',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/msapplication-icon-144x144.png',
-          sizes: '144x144',
-          type: 'image/png'
-        },
-        {
-          src: './img/icons/mstile-150x150.png',
-          sizes: '150x150',
-          type: 'image/png'
-        }
-      ]
-    },
+    appleMobileWebAppStatusBarStyle: 'default',
     workboxOptions: {
       exclude: [/\.map$/, /_redirects/],
       // https://github.com/yyx990803/register-service-worker/issues/14
@@ -99,21 +16,8 @@ module.exports = {
   configureWebpack: {
     optimization: {
       splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: Infinity,
-        minSize: 0,
-        maxSize: 200000,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name (module) {
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1]
-              return `npm.${packageName.replace('@', '')}`
-            }
-          }
-        }
+        minSize: 10000,
+        maxSize: 240000
       }
     }
   },
