8dc92e4770b1406fdc0f81680b30c7b305c67c36
raphaelschnick <raphael.schnick@sybit.de>
2021-11-11 12:32:19 +0100

    tip carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
     <h1>Wie entsorge ich im Landkreis Konstanz &hellip;</h1>
-
     <searchbar :items="materials" class="my-5"/>
+    <tip-carousel />
     <partner />
   </v-container>
 </template>
@@ -10,10 +10,12 @@
 <script>
 import Searchbar from '../components/searchbar/Searchbar'
 import Partner from '../components/partner/Partner'
+import TipCarousel from '../components/tips/Carousel'
 
 export default {
   name: 'Home',
   components: {
+    TipCarousel,
     Searchbar,
     Partner
   },
