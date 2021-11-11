caeff88b28c7ce8cd466ebd11fd5ccc1e8685812
raphaelschnick <raphael.schnick@sybit.de>
2021-11-11 12:39:58 +0100

    update

--- src/components/searchbar/Searchbar.vue
+++ src/components/searchbar/Searchbar.vue
@@ -1,17 +1,15 @@
 <template>
-  <div>
+  <v-toolbar>
     <v-autocomplete
         v-model="select"
         :loading="items && items.length < 0"
         :items="items"
         class="rounded-xl"
         solo
+        no-data-text="Keine Treffer!"
         hide-details
-        no-data-text="Keine Treffer! Drücke auf die Suchlupe, um eine Anfrage zu senden."
         label="Tippe zum Suchen"
-        :filter="filter"
-        @keyup.enter="search()"
-        :return-object="true"
+        :filter="filterObject"
     >
       <template slot="item" slot-scope="data">
         {{ data.item.name }}
@@ -20,60 +18,15 @@
         {{ data.item.name }}
       </template>
       <template v-slot:append>
-        <v-btn icon @click="search">
-          <v-icon>
-            {{ $route.path === "/detail" ? "mdi-magnify" : "mdi-help" }}
-          </v-icon>
-        </v-btn>
+        <v-icon>
+          {{ $route.path === "/detail" ? "mdi-magnify" : "mdi-help" }}
+        </v-icon>
       </template>
     </v-autocomplete>
-    <v-dialog
-        v-model="dialog"
-        persistent
-        max-width="600px"
-        class="rounded-xl"
-    >
-      <v-card class="pa-5 rounded-xl">
-        <v-card-title class="text-h5">
-          Material Anfrage
-        </v-card-title>
-        <v-form ref="form" v-model="valid" @submit.prevent="addMaterial">
-          <v-card-text>
-            <v-text-field v-model="material.name" :rules="nameRules" label="Name" required outlined/>
-            <v-textarea v-model="material.notes" label="Beschreibung" outlined/>
-            <v-alert v-if="message" color="error">
-              {{ message }}
-            </v-alert>
-          </v-card-text>
-          <v-card-actions class="px-4">
-            <v-spacer></v-spacer>
-            <v-btn
-                color="secondary"
-                class="rounded-xl"
-                text
-                @click="dialog = false"
-            >
-              Abbrechen
-            </v-btn>
-            <v-btn
-                :disabled="!valid"
-                :loading="$store.state.showLoadingSpinner"
-                color="primary"
-                type="submit"
-                class="rounded-xl"
-            >
-              Senden
-            </v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-dialog>
-  </div>
+  </v-toolbar>
 </template>
 
 <script>
-import materialService from '../../services/material.service'
-
 export default {
   props: {
     items: {
@@ -84,74 +37,38 @@ export default {
   watch: {
     select () {
       this.search()
-    },
-    dialog () {
-      if (!this.dialog) {
-        this.message = ''
-      }
     }
   },
   data () {
     return {
-      select: null,
-      dialog: false,
-      valid: false,
-      nameRules: [
-        v => !!v || 'Bitte Name eintragen',
-        v => v.length < 50 || 'Der Name darf nicht länger als 50 Zeichen sein.'
-      ],
-      material: {
-        name: '',
-        notes: ''
-      },
-      message: ''
+      select: null
     }
   },
   methods: {
     search () {
       if (this.select?.id) {
         this.$router.push({ name: 'DetailPage', params: { id: this.select.id } })
+      }
+    },
+    filterObject: function (item, queryText) {
+      if (item.synonyms) {
+        return (
+          item.name.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) >
+            -1 ||
+            item.synonyms
+              .toLocaleLowerCase()
+              .indexOf(queryText.toLocaleLowerCase()) > -1
+        )
       } else {
-        this.dialog = true
+        return (
+          item.name.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) >
+            -1
+        )
       }
-    },
-    addMaterial () {
-      if (this.$refs.form.validate()) {
-        this.$store.dispatch('updateShowLoadingSpinner', true)
-        materialService.getAllNames().then(names => {
-          if (!names.includes(this.material.name)) {
-            materialService.addMaterial(this.material)
-            this.dialog = false
-            this.$notify({
-              group: 'default',
-              type: 'success',
-              title: 'Vielen Dank!',
-              text: 'Wir haben deine Anfrage erfolgreich erhalten.'
-            })
-          } else {
-            this.message = 'Dieses Material gibt es bereits'
-          }
-          this.$store.dispatch('updateShowLoadingSpinner', false)
-        })
-      }
-    },
-    filter (item, queryText) {
-      if (item != null && queryText != null) {
-        this.material.name = queryText
-        if (item.synonyms) {
-          return (
-            item.name.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) > -1 ||
-              item.synonyms.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) > -1)
-        } else {
-          return item.name.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) > -1
-        }
-      }
-      return -1
     }
   }
 }
 </script>
-
 <style scoped>
 .v-sheet.v-toolbar:not(.v-sheet--outlined) {
   box-shadow: none !important;
