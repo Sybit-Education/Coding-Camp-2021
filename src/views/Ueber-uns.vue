e893f525ebc5382e142262a5c965518fcd6c31be
Henrik04 <henrik.schmid@freenet.de>
2021-11-11 09:18:17 +0100

    about us page
    improve grammatical and spelling mistakes

--- src/views/Ueber-uns.vue
+++ src/views/Ueber-uns.vue
@@ -8,30 +8,27 @@
       Das Sybit Coding-Camp findet jährlich statt und bietet interessierten
       Jugendlichen die Chance die Ausbildung als Fachinformatiker
       (Anwendungsentwicklung) und den Beruf des Softwareentwicklers genauer
-      kennenzulernen. Die Auszubildenden des Unternehmens "sybitcx" bereiten
+      kennenzulernen. Die Auszubildenden der Firma Sybit bereiten
       hierfür im Vorfeld ein Coding-Projekt für die einwöchige Veranstaltung vor
-      und unterstützen dann die Teilnehmenden Jugendlichen bei ihrer Arbeit 
+      und unterstützen dann die Teilnehmenden Jugendlichen bei ihrer Arbeit
       während des Camps.
     </h4>
     <h2 class="mt-5 d-flex justify-center">
       Was haben wir während des Coding-Camps gemacht?
     </h2>
-    <h4>
-      Während des Coding-Camps lernten wir (zehn Praktikanten und Schüler)
-      vieles über das Coden mit Vue, Java, JavaScript, HTML und CSS.
-      Mit Hilfe von sechs Auszubildenenden des Unternehmens "sybitcx" codeten
-      und entwickelten wir die App "Mülli", eine App, die Nutzer unterstützt ihren
+    <h4 class="mt-5 d-flex justify-center">
+      Während des Coding-Camps lernten wir vieles über das Programmieren mit Vue, JavaScript, HTML und CSS.
+      Gemeinsam mit den Auszubildenden codeten und entwickelten wir die App "Mülli", eine App, die Nutzer unterstützt ihren
       Müll zu trennen und vor allem an den richtigen Orten zu entsorgen.
       Nach einer Gruppeneinteilung wurde uns das "Persona-Modell" erklärt und
       vorgestellt, welche einen Prototyp für eine Gruppe von Nutzern, mit konkret
       ausgeprägten Eigenschaften und einem konkreten Nutzungsverhalten darstellt.
-      Auch die Firma "sybitcx" arbeitet mit solch einem Modell, um ihre Produkte
+      Auch Sybit arbeitet mit solch einem Modell, um ihre Produkte
       und Services bedarfs- und zielgruppenspezifisch zu entwickeln.
       Auch uns unterstützte das Modell der "Personas" sehr, weil wir so anhand der
       ausgedachten User-Storys der "Personas" unsere Ideen zum Projekt "Mülli" nochmals
       überarbeiten und abändern konnten.
-      So entwicklten wir erfolgreich unsere erste, eigene, funktionierende App zur 
-      Müllentsorgung.
+      So entwickelten wir erfolgreich unsere eigene App zur Müllentsorgung.
     </h4>
     <v-img contain class="mt-10" :src="image"></v-img>
   </v-container>
