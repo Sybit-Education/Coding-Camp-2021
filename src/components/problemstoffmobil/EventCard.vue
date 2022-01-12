3e190225e5b2bb59c6ee9439c09b19e0fb1d7866
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-01-12 13:35:17 +0100

    ipruved ux

--- src/components/problemstoffmobil/EventCard.vue
+++ src/components/problemstoffmobil/EventCard.vue
@@ -1,19 +1,37 @@
 <template>
-  <v-card :id="event.id" class="rounded-xl my-3">
+  <v-card :id="event.id" class="event-card rounded-xl my-3">
     <v-card-title>
-      <h4>{{ eventdate }}</h4>
+      <h4>
+        <v-icon class="event-card__calendar-icon" color="primary"
+          >mdi-calendar</v-icon
+        >
+        {{ eventdate }}
+      </h4>
     </v-card-title>
     <v-card-text>
-      <p>
-        <b>Zeitraum:</b>
-        {{ event.timeFrom }} - {{ event.timeTo }}
-      </p>
-      <p>
-        <b>Sammelpunkt:</b><br />
-        {{ event.collectionPoint }}<br />
-        {{ event.municipality
-        }}<span v-if="event.district">, Ortsteil: {{ event.district }}</span>
-      </p>
+      <v-row>
+        <v-col cols="1"
+          ><v-icon size="20" class="event-card__clock-icon"
+            >mdi-clock</v-icon
+          ></v-col
+        >
+        <v-col cols="11">
+          <b>Zeitraum:</b> {{ event.timeFrom }} - {{ event.timeTo }}
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="1"
+          ><v-icon size="20" class="event-card__map-marker-icon"
+            >mdi-map-marker</v-icon
+          ></v-col
+        >
+        <v-col cols="11">
+          <b>Sammelpunkt:</b><br />
+          {{ event.collectionPoint }}<br />
+          {{ event.municipality
+          }}<span v-if="event.district">, Ortsteil: {{ event.district }}</span>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
