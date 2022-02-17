099af30237bd4c5e1d68fc697e0e1e8b0f1724da
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-02-17 08:08:20 +0100

    add maskable attribute: https://web.dev/maskable-icon-audit

--- vue.config.js
+++ vue.config.js
@@ -21,58 +21,69 @@ module.exports = {
         {
           src: './img/icons/android-chrome-192x192.png',
           sizes: '192x192',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/android-chrome-384x384.png',
           sizes: '384x384',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/apple-touch-icon.png',
           sizes: '180x180',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/favicon-16x16.png',
           sizes: '16x16',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/favicon-32x32.png',
           sizes: '32x32',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/mstile-150x150.png',
           sizes: '150x150',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/apple-touch-icon-152x152.png',
           sizes: '152x152',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/apple-touch-icon-167x167.png',
           sizes: '167x167',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/apple-touch-icon-180x180.png',
           sizes: '180x180',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/mstile-270x270.png',
           sizes: '270x270',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any maskable'
         },
         {
           src: './img/icons/android-chrome-512x512.png',
           sizes: '512x512',
-          type: 'image/png'
-        },
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
       ]
     },
     workboxOptions: {
