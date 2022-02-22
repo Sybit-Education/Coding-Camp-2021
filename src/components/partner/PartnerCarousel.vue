8e1f326bfef73db1de10a696d21dfd09eef5e119
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-02-22 14:00:13 +0100

    updated layout

--- src/components/partner/PartnerCarousel.vue
+++ src/components/partner/PartnerCarousel.vue
@@ -3,22 +3,26 @@
     <v-carousel
       v-if="partnerList && partnerList.length > 0"
       class="partner-carousel"
-      height="220"
+      width="auto"
+      height="auto"
       cycle
       hide-delimiter-background
       hide-delimiters
       :show-arrows="false"
     >
-      <v-carousel-item v-for="partner in partnerList" :key="partner.id">
-        <v-sheet class="d-flex justify-center">
-          <router-link :to="`/partner#${partner.id}`">
-            <v-img
-              class="partner-logo"
-              :src="partner.logo[0].url"
-              :alt="partner.name"
-            />
-          </router-link>
-        </v-sheet>
+      <v-carousel-item
+        v-for="partner in partnerList"
+        :key="partner.id"
+        :to="`/partner#${partner.id}`"
+      >
+        <v-card class="rounded-xl d-flex justify-center my-1 mx-1 px-3 py-3">
+          <v-img
+            class="partner-logo"
+            :src="partner.logo[0].url"
+            :alt="partner.name"
+            contain
+          />
+        </v-card>
       </v-carousel-item>
     </v-carousel>
     <v-skeleton-loader v-else type="image" />
@@ -43,8 +47,8 @@ export default {
 </script>
 <style lang="scss" scoped>
 .partner-logo {
-  width: 240px;
-  max-height: 240px;
+  max-width: 400px;
+  height: 220px;
   filter: grayscale(60%);
   &:hover,
   &:focus {
