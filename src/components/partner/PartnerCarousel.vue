106712c7166ecafb3b77f42ea1060c24ea6f8c2c
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-02-23 14:48:45 +0100

    redesigned partner carousell

--- src/components/partner/PartnerCarousel.vue
+++ src/components/partner/PartnerCarousel.vue
@@ -1,32 +1,27 @@
 <template>
-  <div>
-    <v-carousel
-      v-if="partnerList && partnerList.length > 0"
-      class="partner-carousel"
-      width="auto"
-      height="auto"
-      cycle
-      hide-delimiter-background
-      hide-delimiters
-      :show-arrows="false"
+  <v-row class="partner-carousel">
+    <v-col
+      v-for="partner in partnerList"
+      :key="partner.id"
+      cols="6"
+      xs="6"
+      sm="4"
+      md="3"
+      xl="2"
     >
-      <v-carousel-item
-        v-for="partner in partnerList"
-        :key="partner.id"
+      <v-card
+        class="partner-carousel__card rounded-xl d-flex justify-center  px-3"
         :to="`/partner#${partner.id}`"
       >
-        <v-card class="rounded-xl d-flex justify-center my-1 mx-1 px-3 py-3">
-          <v-img
-            class="partner-logo"
-            :src="partner.logo[0].url"
-            :alt="partner.name"
-            contain
-          />
-        </v-card>
-      </v-carousel-item>
-    </v-carousel>
-    <v-skeleton-loader v-else type="image" />
-  </div>
+        <v-img
+          class="partner-logo"
+          :src="partner.logo[0].url"
+          :alt="partner.name"
+          contain
+        />
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 <script>
 import partnerService from '@/services/partner.service'
@@ -46,10 +41,12 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.partner-carousel__card {
+  height: 180px;
+  max-width: 480px;
+}
 .partner-logo {
-  max-width: 400px;
-  height: 220px;
-  filter: grayscale(60%);
+  filter: grayscale(90%);
   &:hover,
   &:focus {
     filter: grayscale(0%);
