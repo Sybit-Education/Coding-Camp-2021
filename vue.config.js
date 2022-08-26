085486c8aa9fb3fd9887f9fa3b70a0b4c4552db9
Stephan Strittmatter <stritti@users.noreply.github.com>
2022-08-26 14:06:52 +0200

    Feature/update versions (#101)
    * chore: update build
    
    * fix: lint errors corrected
    
    * fix: updated dependencies
    
    * fix: add vuetify dependend staff for build

--- vue.config.js
+++ vue.config.js
@@ -74,26 +74,5 @@ module.exports = {
       skipWaiting: true
     }
   },
-  configureWebpack: {
-    optimization: {
-      splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: Infinity,
-        minSize: 0,
-        maxSize: 200000,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name (module) {
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1]
-              return `npm.${packageName.replace('@', '')}`
-            }
-          }
-        }
-      }
-    }
-  },
   transpileDependencies: ['vuetify', 'vuex-persist']
 }
